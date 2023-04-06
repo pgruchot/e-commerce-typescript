2d2b940375fb365c560f6d9bb2e24715b2bfa39c
Patryk Gruchot <patryk.gruchot@outlook.com>
2023-04-06 20:46:51 +0200

    firebase utils, user and cart logic converted to TS

---
+++ src/store/user/user.reducer.ts
@@ -0,0 +1,41 @@
+import { AnyAction } from "redux";
+import { USER_ACTION_TYPES } from "./user.types";
+import {
+  signInFailed,
+  signUpFailed,
+  signOutFailed,
+  signOutSuccess,
+  signInSuccess,
+} from "./user.action";
+
+import { UserData } from "../../utils/firebase/firebase.utils";
+import { act } from "@testing-library/react";
+
+export type UserState = {
+  readonly currentUser: UserData | null;
+  readonly isLoading: boolean;
+  readonly error: Error | null;
+};
+
+const INITIAL_STATE: UserState = {
+  currentUser: null,
+  isLoading: false,
+  error: null,
+};
+
+export const userReducer = (state = INITIAL_STATE, action) => {
+  if (signInSuccess.match(action)) {
+    return { ...state, currentUser: action.payload };
+  }
+  if (signOutSuccess.match(action)) {
+    return { ...state, currentUser: null };
+  }
+  if (
+    signInFailed.match(action) ||
+    signUpFailed.match(action) ||
+    signOutFailed.match(action)
+  ) {
+    return { ...state, error: action.payload };
+  }
+  return state;
+};
