2c7b7f06c20c5d9f7aa132edc00432c9c711763a
Patryk Gruchot <patryk.gruchot@outlook.com>
2023-04-10 01:05:58 +0200

    mobile styling adjustments

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 
 Moving Clothing store project from https://www.udemy.com/course/complete-react-developer-zero-to-mastery/ tutorial into Typescript.
 This version doesn't include redux-toolkit.
+Added PWA functionality.
+Modified mobile styling.
 
 # Project preview
 
--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import Authentication from "./routes/authentication/authentication.component";
 import Shop from "./routes/shop/shop.component";
 import Checkout from "./routes/checkout/checkout.component";
 import { checkUserSession } from "./store/user/user.action";
+import { GlobalStyle } from "./global.styles";
 
 const App = () => {
   const dispatch = useDispatch();
@@ -18,14 +19,16 @@ const App = () => {
   }, []);
 
   return (
-    <Routes>
-      <Route path="/" element={<Navigation />}>
-        <Route index element={<Home />} />
-        <Route path="shop/*" element={<Shop />} />
-        <Route path="auth" element={<Authentication />} />
-        <Route path="checkout" element={<Checkout />} />
-      </Route>
-    </Routes>
+    <GlobalStyle>
+      <Routes>
+        <Route path="/" element={<Navigation />}>
+          <Route index element={<Home />} />
+          <Route path="shop/*" element={<Shop />} />
+          <Route path="auth" element={<Authentication />} />
+          <Route path="checkout" element={<Checkout />} />
+        </Route>
+      </Routes>
+    </GlobalStyle>
   );
 };
 
